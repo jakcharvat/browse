46340c5045f308bb35dcfe5fb9ab53eeb4835ffe
Jakub Charvat <jakcharvat@gmail.com>
2020-05-05 18:39:21 +0200

    Initial Commit

---
+++ Browse/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Browse
+//
+//  Created by Jakub Charvat on 05/05/2020.
+//  Copyright © 2020 jakcharvat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Browse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Browse
+//
+//  Created by Jakub Charvat on 05/05/2020.
+//  Copyright © 2020 jakcharvat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
